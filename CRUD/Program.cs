490f99c1c812a9244dd73fbe48158471cf07e526
Andrey <devandrey98@gmail.com>
2024-06-04 15:26:31 -0600

    feat: added dependency injection configuiration for db context

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,4 +1,10 @@
+using CRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//dependency onjection for db context
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
