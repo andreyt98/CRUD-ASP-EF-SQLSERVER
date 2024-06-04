570ca6b5820ea3fac58415eef3176857e4050722
Andrey <devandrey98@gmail.com>
2024-06-04 14:54:49 -0600

    feat: added user model and properties

---
+++ CRUD/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        
+
+        [Required(ErrorMessage = "this field cannot be empty")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "this field cannot be empty")]
+        public string Email { get; set; }
+        
+
+        [Required(ErrorMessage = "this field cannot be empty")]
+        public string Password { get; set; }
+
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
